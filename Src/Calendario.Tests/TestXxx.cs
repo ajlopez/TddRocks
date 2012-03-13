f9b18574379424cbf0caaff165e64d81ae1d1c7e
ajlopez <webmaster@ajlopez.com>
2012-03-13 19:16:37 -0300

    First Test

---
+++ Src/Calendario.Tests/TestXxx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calendario.Tests
+{
+    [TestClass]
+    public class TestXxx
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            WorkingDatesCalendar argentineCalendar = new WorkingDatesCalendar();
+            Assert.IsTrue(argentineCalendar.IsNonWorkingDate(someSunday());
+        }
+
+        private DateTime someSunday()
+        {
+            return new DateTime(2012, 3, 11);
+        }
+    }
+}
