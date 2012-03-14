a04cfa02d4db2367f44ba288c43df25745a4190d
ajlopez <webmaster@ajlopez.com>
2012-03-14 18:40:11 -0300

    New Version, first test in red

---
+++ MySrc/WorkingDays.Tests/WorkingDaysCalendarTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorkingDays.Tests
+{
+    [TestClass]
+    public class WorkingDaysCalendarTests
+    {
+        [TestMethod]
+        public void TypicalMondayIsAWorkingDay()
+        {
+            WorkingDaysCalendar calendar = new WorkingDaysCalendar();
+            
+            DateTime monday = new DateTime(2012, 3, 12);
+
+            Assert.IsTrue(calendar.IsWorkingDay(monday));
+        }
+    }
+}
---
+++ MySrc/WorkingDays/WorkingDaysCalendar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkingDays
+{
+    public class WorkingDaysCalendar
+    {
+        public bool IsWorkingDay(DateTime day)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
