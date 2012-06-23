0a2c28b26ef8cf067bb4c327ffbea11eb5cb48b7
ajlopez <webmaster@ajlopez.com>
2012-06-23 16:50:56 -0300

    Project Repository with first test

---
+++ TddApp/TddApp.Core.Tests/Data/ProjectRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddApp.Core.Data;
+
+namespace TddApp.Core.Tests.Data
+{
+    [TestClass]
+    public class ProjectRepositoryTests
+    {
+        [TestMethod]
+        public void GetProjects()
+        {
+            ProjectRepository repository = new ProjectRepository();
+
+            var result = repository.GetProjects();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TddApp/TddApp.Core/Data/ProjectRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace TddApp.Core.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using TddApp.Core.Entities;
+
+    public class ProjectRepository
+    {
+        private IList<Project> projects;
+
+        public ProjectRepository()
+            : this(new List<Project>())
+        {
+        }
+
+        public ProjectRepository(IList<Project> projects)
+        {
+            this.projects = projects;
+        }
+
+        public IQueryable<Project> GetProjects()
+        {
+            return this.projects.AsQueryable();
+        }
+    }
+}
