42b85d1c5dd5f1a52bdd44728d2a92d5c378472e
ajlopez <webmaster@ajlopez.com>
2012-06-23 16:36:29 -0300

    ProjectServices with project in constructor, with test

---
+++ TddApp/TddApp.Core.Tests/Services/ProjectServicesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddApp.Core.Services;
+using TddApp.Core.Entities;
+
+namespace TddApp.Core.Tests.Services
+{
+    [TestClass]
+    public class ProjectServicesTests
+    {
+        [TestMethod]
+        public void GetProjects()
+        {
+            ProjectServices services = new ProjectServices();
+
+            var result = services.GetProjects();
+
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void GetThreeProjects()
+        {
+            var projects = new List<Project>()
+            {
+                new Project() { Id = 1, Name = "Project 1" },
+                new Project() { Id = 2, Name = "Project 2" },
+                new Project() { Id = 3, Name = "Project 3" }
+            };
+
+            ProjectServices services = new ProjectServices(projects);
+
+            var result = services.GetProjects();
+
+            Assert.AreEqual(3, result.Count());
+        }
+    }
+}
--- TddApp/TddApp.Core/Services/ProjectServices.cs
+++ TddApp/TddApp.Core/Services/ProjectServices.cs
@@ -4,8 +4,25 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using TddApp.Core.Entities;
 
     public class ProjectServices
     {
+        private IList<Project> projects;
+
+        public ProjectServices()
+            : this(new List<Project>())
+        {
+        }
+
+        public ProjectServices(IList<Project> projects)
+        {
+            this.projects = projects;
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            return this.projects;
+        }
     }
 }
