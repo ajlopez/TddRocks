5e9ff2d59d2180c4e1176b26684f66fee357c9f5
ajlopez <webmaster@ajlopez.com>
2013-03-26 19:19:09 -0300

    First code, Factura GetTotal first test

---
+++ Ventas/Ventas.Tests/FacturaTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ventas.Tests
+{
+    [TestClass]
+    public class FacturaTests
+    {
+        [TestMethod]
+        public void FacturaVaciaTotalEnCero()
+        {
+            Factura factura = new Factura();
+
+            Assert.AreEqual(0, factura.GetTotal());
+        }
+    }
+}
---
+++ Ventas/Ventas/Factura.cs
@@ -0,0 +1,15 @@
+﻿namespace Ventas
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Factura
+    {
+        public decimal GetTotal()
+        {
+            return 0;
+        }
+    }
+}
