1130c242993615d526927f1b7b31715acae1bb95
ajlopez <webmaster@ajlopez.com>
2013-04-10 09:27:14 -0300

    Spock Game, first tests, not compile

---
+++ SpockGame/SpockGameTests/GameTests.cs
@@ -0,0 +1,14 @@
+﻿namespace SpockGameTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void ScissorsCutPaper()
+        {
+            Game game = new Game();
+
+            Assert.AreEqual(PlayResult.FirstPlayer, game.Play(Play.Scissors, Play.Paper));
+        }
+    }
+}
