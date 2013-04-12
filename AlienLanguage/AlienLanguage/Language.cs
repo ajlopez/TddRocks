00d5407f3edebec4970cd0d6307ed2d3075ca1d4
ajlopez <webmaster@ajlopez.com>
2013-04-12 07:04:32 -0300

    Match Unknown Word

---
+++ AlienLanguage/AlienLanguage/Language.cs
@@ -0,0 +1,15 @@
+﻿namespace AlienLanguage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Language
+    {
+        public IEnumerable<string> Match(string match)
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ AlienLanguage/AlienLanguageTests/LanguageTests.cs
@@ -0,0 +1,24 @@
+﻿namespace AlienLanguageTests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AlienLanguage;
+
+    [TestClass]
+    public class LanguageTests
+    {
+        [TestMethod]
+        public void MatchUnknownWord()
+        {
+            Language language = new Language();
+
+            var result = language.Match("abc");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count());
+        }
+    }
+}
