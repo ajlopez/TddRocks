c9d1560b955784c88b3ea0161bbe9f1fcdde62a6
ajlopez <webmaster@ajlopez.com>
2013-04-13 08:29:19 -0300

    Set rows, get cell

--- Lawnmover/Lawnmover/Pattern.cs
+++ Lawnmover/Lawnmover/Pattern.cs
@@ -9,11 +9,24 @@
     {
         private int width;
         private int height;
+        private int[,] cells;
 
         public Pattern(int width, int height)
         {
             this.width = width;
             this.height = height;
+            this.cells = new int[width, height];
+        }
+
+        public void SetRow(int nrow, IList<int> values)
+        {
+            for (int k = 0; k < width; k++)
+                this.cells[nrow, k] = values[k];
+        }
+
+        public int GetCell(int nrow, int ncol)
+        {
+            return this.cells[nrow, ncol];
         }
 
         public int Width { get { return this.width; } }
--- Lawnmover/LawnmoverTests/PatternTests.cs
+++ Lawnmover/LawnmoverTests/PatternTests.cs
@@ -18,5 +18,19 @@
             Assert.AreEqual(3, pattern.Width);
             Assert.AreEqual(2, pattern.Height);
         }
+
+        [TestMethod]
+        public void SetRows()
+        {
+            Pattern pattern = new Pattern(2, 2);
+
+            pattern.SetRow(0, new int[] { 1, 2 });
+            pattern.SetRow(1, new int[] { 2, 3 });
+
+            Assert.AreEqual(1, pattern.GetCell(0, 0));
+            Assert.AreEqual(2, pattern.GetCell(0, 1));
+            Assert.AreEqual(2, pattern.GetCell(1, 0));
+            Assert.AreEqual(3, pattern.GetCell(1, 1));
+        }
     }
 }
