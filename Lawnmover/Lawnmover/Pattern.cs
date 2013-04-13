fe1213b591d4fe05d0da5dee1daf1154e6afa8f6
ajlopez <webmaster@ajlopez.com>
2013-04-13 08:26:04 -0300

    Lawnmover first test

---
+++ Lawnmover/Lawnmover/Pattern.cs
@@ -0,0 +1,23 @@
+﻿namespace Lawnmover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Pattern
+    {
+        private int width;
+        private int height;
+
+        public Pattern(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width { get { return this.width; } }
+
+        public int Height { get { return this.height; } }
+    }
+}
---
+++ Lawnmover/LawnmoverTests/PatternTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LawnmoverTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Lawnmover;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PatternTests
+    {
+        [TestMethod]
+        public void CreatePattern()
+        {
+            Pattern pattern = new Pattern(3, 2);
+
+            Assert.AreEqual(3, pattern.Width);
+            Assert.AreEqual(2, pattern.Height);
+        }
+    }
+}
