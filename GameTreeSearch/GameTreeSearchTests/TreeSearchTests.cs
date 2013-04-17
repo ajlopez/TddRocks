7a7564b528ea7777022f30ed255191c61fd46ad2
ajlopez <webmaster@ajlopez.com>
2013-04-17 07:34:21 -0300

    GameTreeSearch, initial commit, test doesn't compile yet

---
+++ GameTreeSearch/GameTreeSearchTests/TreeSearchTests.cs
@@ -0,0 +1,21 @@
+﻿namespace GameTreeSearchTests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TreeSearchTests
+    {
+        [TestMethod]
+        public void DepthZeroReturnRootNode()
+        {
+            Tree tree = new Tree(3);
+            TreeSearch search = new TreeSearch();
+
+            Assert.AreEqual(3, search.FindMaxValue(tree));
+        }
+    }
+}
