c9314a87d9622f723c88a11a4f90c84fa2865edb
ajlopez <webmaster@ajlopez.com>
2013-04-26 22:49:04 -0300

    Bulleyes with new algorithm

---
+++ Bulleyes/Bulleyes.Tests/PaintTests.cs
@@ -0,0 +1,68 @@
+﻿namespace Bulleyes.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PaintTests
+    {
+        [TestMethod]
+        public void PaintForRingRadiusOne()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(3, paint.PaintFor(1));
+        }
+
+        [TestMethod]
+        public void PaintForRingRadiusTwo()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(5, paint.PaintFor(2));
+        }
+
+        [TestMethod]
+        public void CountForRadiusOnePaintNine()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(1, paint.CountFor(1, 9));
+        }
+
+        [TestMethod]
+        public void CountForRadiusOnePaintTen()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(2, paint.CountFor(1, 10));
+        }
+
+        [TestMethod]
+        public void CountForRadiusThreePaintForty()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(3, paint.CountFor(3, 40));
+        }
+
+        [TestMethod]
+        public void CountForRadiusOnePaint1000000000000000000()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(707106780, paint.CountFor(1, 1000000000000000000));
+        }
+
+        [TestMethod]
+        public void CountForRadius10000000000000000Paint1000000000000000000()
+        {
+            Paint paint = new Paint();
+
+            Assert.AreEqual(49, paint.CountFor(10000000000000000, 1000000000000000000));
+        }
+    }
+}
---
+++ Bulleyes/Bulleyes/Paint.cs
@@ -0,0 +1,47 @@
+﻿namespace Bulleyes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Paint
+    {
+        public int PaintFor(int radius)
+        {
+            return (radius + 1) * (radius + 1) - radius * radius;
+        }
+
+        public long CountFor(long radius, long paint)
+        {
+            long rest = paint;
+            long count = 0;
+            long incr = 1;
+
+            while (true)
+            {
+                while ((count + incr) * (2 * radius + 1 + 2 * (count + incr - 1)) <= paint)
+                {
+                    count += incr;
+                    incr *= 2;
+                }
+
+                if (incr == 1)
+                    break;
+
+                incr /= 2;
+            }
+
+            return count;
+
+            //for (int p = this.PaintFor(radius); p <= rest; p = this.PaintFor(radius))
+            //{
+            //    rest -= p;
+            //    count++;
+            //    radius += 2;
+            //}
+
+            //return count;
+        }
+    }
+}
