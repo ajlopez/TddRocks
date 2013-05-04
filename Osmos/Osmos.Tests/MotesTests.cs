13b3ada9d5a4243e8d2be1f2fa7d6fd040c1ed06
ajlopez <webmaster@ajlopez.com>
2013-05-04 13:18:08 -0300

    Osmos first tests

---
+++ Osmos/Osmos.Tests/MotesTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Osmos.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MotesTests
+    {
+        [TestMethod]
+        public void OneSmallerMoteCanBeSolved()
+        {
+            Motes motes = new Motes();
+
+            Assert.IsTrue(motes.CanBeSolved(10, new int[] { 9 }));
+        }
+
+        [TestMethod]
+        public void OneEqualMoteCannotBeSolved()
+        {
+            Motes motes = new Motes();
+
+            Assert.IsFalse(motes.CanBeSolved(10, new int[] { 10 }));
+        }
+
+        [TestMethod]
+        public void OneEqualGreaterCannotBeSolved()
+        {
+            Motes motes = new Motes();
+
+            Assert.IsFalse(motes.CanBeSolved(10, new int[] { 11 }));
+        }
+
+        [TestMethod]
+        public void TwoMotesThanCanBeSolved()
+        {
+            Motes motes = new Motes();
+
+            Assert.IsTrue(motes.CanBeSolved(10, new int[] { 9, 18 }));
+        }
+
+        [TestMethod]
+        public void TwoMotesThanCannotBeSolved()
+        {
+            Motes motes = new Motes();
+
+            Assert.IsFalse(motes.CanBeSolved(10, new int[] { 9, 19 }));
+        }
+    }
+}
---
+++ Osmos/Osmos/Motes.cs
@@ -0,0 +1,24 @@
+﻿namespace Osmos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Motes
+    {
+        public bool CanBeSolved(int mote, IList<int> values)
+        {
+            int count = values.Count;
+
+            for (int k = 0; k < count; k++)
+            {
+                if (mote <= values[k])
+                    return false;
+                mote += values[k];
+            }
+
+            return true;
+        }
+    }
+}
