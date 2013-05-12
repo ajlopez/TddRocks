df9aabae0d7b8366df132e4ea6bc76b83bb4f7aa
ajlopez <webmaster@ajlopez.com>
2013-05-12 07:18:21 -0300

    GreatWall, first algorithm for wall with tests

---
+++ GreatWall/GreatWall.Tests/WallTests.cs
@@ -0,0 +1,69 @@
+﻿namespace GreatWall.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WallTests
+    {
+        [TestMethod]
+        public void AttackInitialWall()
+        {
+            Wall wall = new Wall();
+            wall.NewDay();
+            Assert.IsTrue(wall.Attack(10, 12, 6));
+        }
+
+        [TestMethod]
+        public void AttackInitialWallTwice()
+        {
+            Wall wall = new Wall();
+            wall.NewDay();
+            Assert.IsTrue(wall.Attack(10, 12, 6));
+            Assert.IsTrue(wall.Attack(10, 12, 6));
+        }
+
+        [TestMethod]
+        public void AttackInitialWallTwoDays()
+        {
+            Wall wall = new Wall();
+            wall.NewDay();
+            Assert.IsTrue(wall.Attack(10, 12, 6));
+            wall.NewDay();
+            Assert.IsFalse(wall.Attack(10, 12, 6));
+        }
+
+        [TestMethod]
+        public void AttackInitialThousandDays()
+        {
+            Wall wall = new Wall();
+
+            for (int k = 0; k < 1000; k++)
+            {
+                wall.NewDay();
+                Assert.IsTrue(wall.Attack(k, k + 100, 6));
+            }
+        }
+
+        [TestMethod]
+        public void AttackInitialThousandDaysTwice()
+        {
+            Wall wall = new Wall();
+
+            for (int k = 0; k < 1000; k++)
+            {
+                wall.NewDay();
+                Assert.IsTrue(wall.Attack(k, k + 100, 6));
+            }
+
+            for (int k = 0; k < 1000; k++)
+            {
+                wall.NewDay();
+                Assert.IsFalse(wall.Attack(k, k + 100, 6));
+            }
+        }
+    }
+}
---
+++ GreatWall/GreatWall/Wall.cs
@@ -0,0 +1,43 @@
+﻿namespace GreatWall
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Wall
+    {
+        private IDictionary<int, int> state = new Dictionary<int, int>();
+        private IDictionary<int, int> newstate;
+
+        public void NewDay()
+        {
+            if (this.newstate != null)
+                this.EndOfDay();
+
+            this.newstate = new Dictionary<int, int>();
+        }
+
+        public void EndOfDay()
+        {
+            foreach (int key in this.newstate.Keys)
+                if (!this.state.ContainsKey(key) || this.state[key] < this.newstate[key])
+                    this.state[key] = this.newstate[key];
+        }
+
+        public bool Attack(int from, int to, int height)
+        {
+            bool result = false;
+
+            for (int k = from; k <= to; k++)
+                if (!this.state.ContainsKey(k) || this.state[k] < height)
+                {
+                    if (!this.newstate.ContainsKey(k) || this.newstate[k] < height)
+                        this.newstate[k] = height;
+                    result = true;
+                }
+
+            return result;
+        }
+    }
+}
