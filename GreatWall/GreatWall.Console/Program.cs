470c28f78dfc2303b626dbc51029f42ab61461f9
ajlopez <webmaster@ajlopez.com>
2013-05-12 08:10:34 -0300

    Great Wall Console

---
+++ GreatWall/GreatWall.Console/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace GreatWall.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputname = args[0];
+            string outputname = args[1];
+
+            string[] lines = File.ReadAllLines(inputname);
+
+            int ncases = int.Parse(lines[0]);
+            int nline = 0;
+            IList<string> results = new List<string>();
+            Attacks attacks = new Attacks();
+
+            for (int k = 0; k < ncases; k++)
+            {
+                Console.WriteLine(string.Format("Solving Case #{0}", k + 1));
+                nline++;
+                int ntribes = int.Parse(lines[nline]);
+                IList<Tribe> tribes = new List<Tribe>();
+
+                for (int j = 0; j < ntribes; j++)
+                {
+                    nline++;
+                    string[] words = lines[nline].Split(' ');
+                    int day = int.Parse(words[0]);
+                    int nattacks = int.Parse(words[1]);
+                    int west = int.Parse(words[2]);
+                    int east = int.Parse(words[3]);
+                    int strength = int.Parse(words[4]);
+                    int ddays = int.Parse(words[5]);
+                    int ddistance = int.Parse(words[6]);
+                    int dstrength = int.Parse(words[7]);
+                    Tribe tribe = new Tribe(day, nattacks, west, east, strength, ddays, ddistance, dstrength);
+                    tribes.Add(tribe);
+                }
+
+                int count = attacks.Count(tribes);
+
+                results.Add(string.Format("Case #{0}: {1}", k + 1, count));
+            }
+
+            File.WriteAllLines(outputname, results.ToArray());
+        }
+    }
+}
--- GreatWall/GreatWall.Tests/AttacksTests.cs
+++ GreatWall/GreatWall.Tests/AttacksTests.cs
@@ -29,5 +29,16 @@
             Attacks attacks = new Attacks();
             Assert.AreEqual(5, attacks.Count(new Tribe[] { new Tribe(0, 3, 0, 2, 10, 2, 3, -2), new Tribe(10, 3, 2, 3, 8, 7, 2, 0) }));
         }
+
+        [TestMethod]
+        public void GoogleSampleThreeTribes()
+        {
+            Attacks attacks = new Attacks();
+            Assert.AreEqual(6, attacks.Count(new Tribe[] {
+                new Tribe(1, 2, 0, 5, 10, 2, 8, 0), 
+                new Tribe(0, 3, 0, 1, 7, 1, 2, 2),
+                new Tribe(3, 3, 0, 5, 1, 1, 4, 0)
+            }));
+        }
     }
 }
