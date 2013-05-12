92b9ca0f71d71cf71cb49d3c5e109c92abe1a391
ajlopez <webmaster@ajlopez.com>
2013-05-12 06:16:18 -0300

    First tests

---
+++ Consonants/Consonants.Tests/AnalyzerTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Consonants.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AnalyzerTests
+    {
+        [TestMethod]
+        public void GetConsonantStringsLength1Sample1()
+        {
+            Analyzer analyzer = new Analyzer();
+            var result = analyzer.GetConsonantPositions("ab", 1);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result[0]);
+        }
+
+        [TestMethod]
+        public void GetConsonantStringsLength1Sample2()
+        {
+            Analyzer analyzer = new Analyzer();
+            var result = analyzer.GetConsonantPositions("bc", 1);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod]
+        public void GetConsonantStringsLength2Sample1()
+        {
+            Analyzer analyzer = new Analyzer();
+            var result = analyzer.GetConsonantPositions("abc", 2);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result[0]);
+        }
+    }
+}
---
+++ Consonants/Consonants/Analyzer.cs
@@ -0,0 +1,32 @@
+﻿namespace Consonants
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Analyzer
+    {
+        private static char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+        public IList<int> GetConsonantPositions(string text, int length)
+        {
+            IList<int> positions = new List<int>();
+            int l = text.Length;
+
+            for (int k = 0; k + length <= l; k++)
+            {
+                int counter = 0;
+
+                for (int j = 0; j < length; j++)
+                    if (!vowels.Contains(text[k + j]))
+                        counter++;
+
+                if (counter == length)
+                    positions.Add(k);
+            }
+
+            return positions;
+        }
+    }
+}
