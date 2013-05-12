afac529db9817c0f22dc8ee91d8998ca13862150
ajlopez <webmaster@ajlopez.com>
2013-05-12 07:32:11 -0300

    Tribe with first tests

---
+++ GreatWall/GreatWall.Tests/TribeTests.cs
@@ -0,0 +1,50 @@
+﻿namespace GreatWall.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TribeTests
+    {
+        [TestMethod]
+        public void CreateTribe()
+        {
+            Tribe tribe = new Tribe(10, 10, 2, 10, 6, 1, 1, 1);
+
+            Assert.IsTrue(tribe.CanAttack());
+            Assert.AreEqual(10, tribe.Day);
+            Assert.AreEqual(6, tribe.Strength);
+            Assert.AreEqual(2, tribe.West);
+            Assert.AreEqual(10, tribe.East);
+        }
+
+        [TestMethod]
+        public void CreateTribeAndNewDay()
+        {
+            Tribe tribe = new Tribe(10, 10, 2, 10, 6, 1, 2, 3);
+
+            tribe.NewDay();
+
+            Assert.IsTrue(tribe.CanAttack());
+            Assert.AreEqual(11, tribe.Day);
+            Assert.AreEqual(9, tribe.Strength);
+            Assert.AreEqual(4, tribe.West);
+            Assert.AreEqual(12, tribe.East);
+        }
+
+        [TestMethod]
+        public void CannotAttack()
+        {
+            Tribe tribe = new Tribe(10, 3, 2, 10, 6, 1, 2, 3);
+
+            tribe.NewDay();
+            tribe.NewDay();
+            Assert.IsTrue(tribe.CanAttack());
+            tribe.NewDay();
+            Assert.IsFalse(tribe.CanAttack());
+        }
+    }
+}
---
+++ GreatWall/GreatWall/Tribe.cs
@@ -0,0 +1,54 @@
+﻿namespace GreatWall
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tribe
+    {
+        private int day;
+        private int nattacks;
+        private int west;
+        private int east;
+        private int strength;
+        private int ddays;
+        private int ddistance;
+        private int dstrength;
+        private int nday;
+
+        public Tribe(int day, int nattacks, int west, int east, int strength, int ddays, int ddistance, int dstrength)
+        {
+            this.day = day;
+            this.nattacks = nattacks;
+            this.west = west;
+            this.east = east;
+            this.strength = strength;
+            this.ddays = ddays;
+            this.ddistance = ddistance;
+            this.dstrength = dstrength;
+        }
+
+        public int Day { get { return this.day; } }
+
+        public int Strength { get { return this.strength; } }
+
+        public int West { get { return this.west; } }
+
+        public int East { get { return this.east; } }
+
+        public bool CanAttack()
+        {
+            return this.nday < this.nattacks;
+        }
+
+        public void NewDay()
+        {
+            this.nday++;
+            this.day += this.ddays;
+            this.west += this.ddistance;
+            this.east += this.ddistance;
+            this.strength += this.dstrength;
+        }
+    }
+}
