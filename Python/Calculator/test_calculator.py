ed8596e2aafeb59461f2d11e789f2efd35569c82
ajlopez <webmaster@ajlopez.com>
2013-06-23 11:18:30 -0300

    Second test in red

--- Python/Calculator/test_calculator.py
+++ Python/Calculator/test_calculator.py
@@ -3,10 +3,15 @@ import unittest
 from calculator import Calculator
 
 class CalculatorTests(unittest.TestCase):
-    def test_add_two_integers(self):
+    def test_add_two_numbers(self):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(5, result)
+
+    def test_subtract_two_numbers(self):
+        calc = Calculator()
+        result = calc.subtract(2, 3)
+        self.assertEqual(-1, result)
 
 if __name__ == '__main__':
     unittest.main()
