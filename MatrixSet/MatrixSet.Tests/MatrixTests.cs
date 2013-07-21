4d72cbb70392d3e43a4c55c4289f0e8553c26e44
ajlopez <webmaster@ajlopez.com>
2013-07-21 16:09:27 -0300

    Matrix first test

---
+++ MatrixSet/MatrixSet.Tests/MatrixTests.cs
@@ -0,0 +1,25 @@
+﻿namespace MatrixSet.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        public void CreateEmptyMatrix()
+        {
+            Matrix matrix = new Matrix(3, 4);
+
+            Assert.AreEqual(3, matrix.Width);
+            Assert.AreEqual(4, matrix.Height);
+
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 4; y++)
+                    Assert.IsFalse(matrix.Get(x, y));
+        }
+    }
+}
---
+++ MatrixSet/MatrixSet/Matrix.cs
@@ -0,0 +1,30 @@
+﻿namespace MatrixSet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Matrix
+    {
+        private int width;
+        private int height;
+        private bool[,] values;
+
+        public Matrix(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.values = new bool[width, height];
+        }
+
+        public int Width { get { return this.width; } }
+
+        public int Height { get { return this.height; } }
+
+        public bool Get(int x, int y)
+        {
+            return this.values[x, y];
+        }
+    }
+}
