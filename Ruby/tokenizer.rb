745ec5923fa965c463cf3fb6a8fdcdf6a6b5926e
ajlopez <webmaster@ajlopez.com>
2013-08-13 20:29:14 -0300

    Tokenizer redesign

--- Ruby/tokenizer.rb
+++ Ruby/tokenizer.rb
@@ -2,37 +2,41 @@
 require "test/unit"
 
 class Tokenizer
-    def getTokens(text)
-        text.split
+    def initialize(text)
+        @text = text
+    end
+    
+    def getTokens()
+        @text.split
     end
 end
 
 class TestTokenizer < Test::Unit::TestCase
  
   def test_get_simple_letter
-    tokenizer = Tokenizer.new()
-    result = tokenizer.getTokens("k")
+    tokenizer = Tokenizer.new("k")
+    result = tokenizer.getTokens()
     assert_equal(1, result.length)
     assert_equal("k", result[0])
   end
  
   def test_get_simple_word
-    tokenizer = Tokenizer.new()
-    result = tokenizer.getTokens("foo")
+    tokenizer = Tokenizer.new("foo")
+    result = tokenizer.getTokens()
     assert_equal(1, result.length)
     assert_equal("foo", result[0])
   end 
  
   def test_get_simple_word_skipping_spaces
-    tokenizer = Tokenizer.new()
-    result = tokenizer.getTokens("  foo   ")
+    tokenizer = Tokenizer.new("  foo   ")
+    result = tokenizer.getTokens()
     assert_equal(1, result.length)
     assert_equal("foo", result[0])
   end 
  
   def test_get_two_simple_word
-    tokenizer = Tokenizer.new()
-    result = tokenizer.getTokens("foo bar")
+    tokenizer = Tokenizer.new("foo bar")
+    result = tokenizer.getTokens()
     assert_equal(2, result.length)
     assert_equal("foo", result[0])
     assert_equal("bar", result[1])
