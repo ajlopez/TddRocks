13fe1ff02e42f49c832e8fa920393bf1b6b1de77
ajlopez <webmaster@ajlopez.com>
2013-11-05 18:35:29 -0300

    get by id, get all

--- JavaScript/workorders/lib/workorders.js
+++ JavaScript/workorders/lib/workorders.js
@@ -1,10 +1,29 @@
 
 function Repository() {
     var maxid = 0;
+    var items = [];
     
     this.add = function (name) {
         var wo = { id: ++maxid, name: name };
+        items[wo.id] = wo;
         return wo;
+    }
+    
+    this.getById = function (id) {
+        if (items[id])
+            return items[id];
+            
+        return null;
+    }
+    
+    this.getAll = function () {
+        var result = [];
+        
+        Object.keys(items).forEach(function (id) {
+            result.push(items[id]);
+        });
+        
+        return result;
     }
 }
 
--- JavaScript/workorders/test/workorders.js
+++ JavaScript/workorders/test/workorders.js
@@ -9,3 +9,38 @@ exports['add workorder'] = function (test) {
     test.equal(wo.id, 1);
     test.equal(wo.name, 'My work order');
 };
+
+exports['get by id'] = function (test) {
+    var repo = workorders.createRepository();
+    repo.add('My work order');
+    
+    var wo = repo.getById(1);
+    
+    test.ok(wo);
+    test.equal(wo.id, 1);
+    test.equal(wo.name, 'My work order');
+};
+
+exports['get unknown by id'] = function (test) {
+    var repo = workorders.createRepository();
+    
+    var wo = repo.getById(1);
+    
+    test.strictEqual(wo, null);
+};
+
+exports['get all'] = function (test) {
+    var repo = workorders.createRepository();
+    repo.add("First workorder");
+    repo.add("Second workorder");
+    
+    var all = repo.getAll();
+    
+    test.ok(all);
+    test.ok(Array.isArray(all));
+    test.equal(all.length, 2);
+    test.equal(all[0].id, 1);
+    test.equal(all[0].name, "First workorder");
+    test.equal(all[1].id, 2);
+    test.equal(all[1].name, "Second workorder");
+};
