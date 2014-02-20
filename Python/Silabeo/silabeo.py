dc60c9b4977bfde16935c7c1f104598ccd740410
ajlopez <webmaster@ajlopez.com>
2014-02-19 21:09:46 -0300

    Two syllabes

--- Python/Silabeo/silabeo.py
+++ Python/Silabeo/silabeo.py
@@ -1,4 +1,23 @@
 
 class Silabeador:
     def parse(self, text):
-        return [text]
+        result = []
+        syllabe = ""
+        
+        for k in range(len(text)):
+            letter = text[k]
+            if is_vowel(letter):
+                syllabe += letter
+            else:
+                if len(syllabe) > 0:
+                    result.append(syllabe)
+                syllabe = letter
+                
+        if len(syllabe) > 0:
+            result.append(syllabe)
+            
+        return result
+        
+
+def is_vowel(letter):
+    return letter in "a"
--- Python/Silabeo/test_silabeo.py
+++ Python/Silabeo/test_silabeo.py
@@ -9,5 +9,19 @@ class SilabeadorTests(unittest.TestCase):
         self.assertEqual(1, len(result))
         self.assertEqual("a", result[0])
 
+    def test_consonant_vowel(self):
+        sil = Silabeador()
+        result = sil.parse("la")
+        self.assertEqual(1, len(result))
+        self.assertEqual("la", result[0])
+
+    def test_consonant_vowel_consonant_vowel(self):
+        sil = Silabeador()
+        result = sil.parse("sala")
+        print(result)
+        self.assertEqual(2, len(result))
+        self.assertEqual("sa", result[0])
+        self.assertEqual("la", result[1])
+
 if __name__ == '__main__':
     unittest.main()
