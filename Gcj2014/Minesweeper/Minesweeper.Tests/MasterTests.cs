995287ab99725f5de593cc621fe7604bb557ac7d
ajlopez <webmaster@ajlopez.com>
2014-04-12 10:47:53 -0300

    Minesweeper, impossible cases test

---
+++ Gcj2014/Minesweeper/Minesweeper.Tests/MasterTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Minesweeper.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MasterTests
+    {
+        [TestMethod]
+        public void SolveImpossibleCases()
+        {
+            Master master = new Master();
+
+            Assert.AreEqual("Impossible", master.Solve(5, 5, 23));
+            Assert.AreEqual("Impossible", master.Solve(2, 2, 1));
+        }
+    }
+}
---
+++ Gcj2014/Minesweeper/Minesweeper/Master.cs
@@ -0,0 +1,36 @@
+﻿namespace Minesweeper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Master
+    {
+        public object Solve(int nrows, int ncols, int nmines)
+        {
+            if (nrows * ncols - nmines <= 4)
+                return "Impossible";
+
+            for (int k = nrows; k >= 2; k--)
+            {
+                int j = nmines / k;
+
+                if (j < 1)
+                    continue;
+
+                int size = k * j;
+
+                if ((nmines % size) % 2 == 0)
+                    return Generate(nrows, ncols, k, j, size);
+            }
+
+            return "Impossible";
+        }
+
+        private object Generate(int nrows, int ncols, int height, int width, int size)
+        {
+            return null;
+        }
+    }
+}
