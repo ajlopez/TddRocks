0e29038e606588bc32d3c4aaf29202d6de2ed2fa
ajlopez <webmaster@ajlopez.com>
2014-04-12 10:18:55 -0300

    Magician resolve tests

---
+++ Gcj2014/MagicTrick/MagicTrick.Tests/MagicianTests.cs
@@ -0,0 +1,70 @@
+﻿namespace MagicTrick.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MagicianTests
+    {
+        [TestMethod]
+        public void SolveSampleCaseOne()
+        {
+            Grid grid1 = new Grid(new string[] {
+                "1 2 3 4",
+                "5 6 7 8",
+                "9 10 11 12",
+                "13 14 15 16"
+            });
+
+            Grid grid2 = new Grid(new string[] {
+                "1 2 5 4",
+                "3 11 6 15",
+                "9 10 7 12",
+                "13 14 8 16"
+            });
+
+            Magician magician = new Magician();
+
+            var result = magician.Resolve(2, grid1, 3, grid2);
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void SolveSampleCaseTwo()
+        {
+            Grid grid1 = new Grid(new string[] {
+                "1 2 3 4",
+                "5 6 7 8",
+                "9 10 11 12",
+                "13 14 15 16"
+            });
+
+            Magician magician = new Magician();
+
+            var result = magician.Resolve(2, grid1, 2, grid1);
+
+            Assert.AreEqual("Bad magician!", result);
+        }
+
+        [TestMethod]
+        public void SolveSampleCaseThree()
+        {
+            Grid grid1 = new Grid(new string[] {
+                "1 2 3 4",
+                "5 6 7 8",
+                "9 10 11 12",
+                "13 14 15 16"
+            });
+
+            Magician magician = new Magician();
+
+            var result = magician.Resolve(2, grid1, 3, grid1);
+
+            Assert.AreEqual("Volunteer cheated!", result);
+        }
+    }
+}
---
+++ Gcj2014/MagicTrick/MagicTrick/Magician.cs
@@ -0,0 +1,26 @@
+﻿namespace MagicTrick
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Magician
+    {
+        public object Resolve(int nrow1, Grid grid1, int nrow2, Grid grid2)
+        {
+            var row1 = grid1.GetRow(nrow1);
+            var row2 = grid2.GetRow(nrow2);
+
+            var result = row1.Intersect(row2).ToList();
+
+            if (result.Count == 0)
+                return "Volunteer cheated!";
+
+            if (result.Count > 1)
+                return "Bad magician!";
+
+            return result[0];
+        }
+    }
+}
