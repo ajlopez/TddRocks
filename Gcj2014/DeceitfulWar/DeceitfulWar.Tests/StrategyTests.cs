4cd87ca4a8e6dec800cfc1bf985489bb4667f823
ajlopez <webmaster@ajlopez.com>
2014-04-12 15:58:36 -0300

    DeceitfulWar, war first tests

---
+++ Gcj2014/DeceitfulWar/DeceitfulWar.Tests/StrategyTests.cs
@@ -0,0 +1,32 @@
+﻿namespace DeceitfulWar.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StrategyTests
+    {
+        [TestMethod]
+        public void CalculateSimpleWarWin()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateWar(new double[] { 0.2 }, new double[] { 0.1 });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void CalculateSimpleWarLost()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateWar(new double[] { 0.1 }, new double[] { 0.2 });
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ Gcj2014/DeceitfulWar/DeceitfulWar/Strategy.cs
@@ -0,0 +1,35 @@
+﻿namespace DeceitfulWar
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Strategy
+    {
+        public int CalculateWar(IList<double> first, IList<double> second)
+        {
+            if (first.Count == 0)
+                return 0;
+
+            double play1 = first.Max();
+            double play2;
+            bool win = false;
+
+            var greater = second.Where(n => n > play1).ToList();
+
+            if (greater.Count > 0)
+                play2 = greater.Min();
+            else
+            {
+                play2 = second.Min();
+                win = true;
+            }
+            
+            var newfirst = first.Where(n => n != play1).ToList();
+            var newsecond = second.Where(n => n != play2).ToList();
+
+            return CalculateWar(newfirst, newsecond) + (win ? 1 : 0);
+        }
+    }
+}
