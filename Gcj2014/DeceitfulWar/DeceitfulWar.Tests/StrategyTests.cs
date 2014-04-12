822e6c052a001f4ca19956b8682f82e39108c8f8
ajlopez <webmaster@ajlopez.com>
2014-04-12 16:08:57 -0300

    Deceitful War cases tests

--- Gcj2014/DeceitfulWar/DeceitfulWar.Tests/StrategyTests.cs
+++ Gcj2014/DeceitfulWar/DeceitfulWar.Tests/StrategyTests.cs
@@ -24,9 +24,39 @@
         {
             Strategy strat = new Strategy();
 
+            var result = strat.CalculateWar(new double[] { 0.5 }, new double[] { 0.6 });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CalculateDeceitfulWarCaseOne()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateDeceitfulWar(new double[] { 0.5 }, new double[] { 0.6 });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CalculateWarCaseTwo()
+        {
+            Strategy strat = new Strategy();
+
             var result = strat.CalculateWar(new double[] { 0.7, 0.2 }, new double[] { 0.8, 0.3 });
 
             Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CalculateDeceitfulWarCaseTwo()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateDeceitfulWar(new double[] { 0.7, 0.2 }, new double[] { 0.8, 0.3 });
+
+            Assert.AreEqual(1, result);
         }
 
         [TestMethod]
@@ -40,6 +70,16 @@
         }
 
         [TestMethod]
+        public void CalculateDeceitfulWarCaseThree()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateDeceitfulWar(new double[] { 0.5, 0.1, 0.9 }, new double[] { 0.6, 0.4, 0.3 });
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
         public void CalculateWarCaseFour()
         {
             Strategy strat = new Strategy();
@@ -47,6 +87,16 @@
             var result = strat.CalculateWar(new double[] { 0.186, 0.389, 0.907, 0.832, 0.959, 0.557, 0.300, 0.992, 0.899 }, new double[] { 0.916, 0.728, 0.271, 0.520, 0.700, 0.521, 0.215, 0.341, 0.458 });
 
             Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void CalculateDeceitfulWarCaseFour()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateDeceitfulWar(new double[] { 0.186, 0.389, 0.907, 0.832, 0.959, 0.557, 0.300, 0.992, 0.899 }, new double[] { 0.916, 0.728, 0.271, 0.520, 0.700, 0.521, 0.215, 0.341, 0.458 });
+
+            Assert.AreEqual(8, result);
         }
 
         [TestMethod]
--- Gcj2014/DeceitfulWar/DeceitfulWar/Strategy.cs
+++ Gcj2014/DeceitfulWar/DeceitfulWar/Strategy.cs
@@ -31,5 +31,35 @@
 
             return CalculateWar(newfirst, newsecond) + (win ? 1 : 0);
         }
+
+        public int CalculateDeceitfulWar(IList<double> first, IList<double> second)
+        {
+            if (first.Count == 0)
+                return 0;
+
+            double min1 = first.Min();
+            double min2 = second.Min();
+            bool win = false;
+
+            double play1;
+            double play2;
+
+            if (min1 < min2)
+            {
+                play1 = min1;
+                play2 = second.Max();
+            }
+            else
+            {
+                play1 = min1;
+                play2 = min2;
+                win = true;
+            }
+
+            var newfirst = first.Where(n => n != play1).ToList();
+            var newsecond = second.Where(n => n != play2).ToList();
+
+            return CalculateDeceitfulWar(newfirst, newsecond) + (win ? 1 : 0);
+        }
     }
 }
