c0fd403770b44df0db080f82d9a5f7fe73138443
ajlopez <webmaster@ajlopez.com>
2014-04-12 16:02:58 -0300

    War cases tests

--- Gcj2014/DeceitfulWar/DeceitfulWar.Tests/StrategyTests.cs
+++ Gcj2014/DeceitfulWar/DeceitfulWar.Tests/StrategyTests.cs
@@ -20,6 +20,36 @@
         }
 
         [TestMethod]
+        public void CalculateWarCaseOne()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateWar(new double[] { 0.7, 0.2 }, new double[] { 0.8, 0.3 });
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CalculateWarCaseThree()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateWar(new double[] { 0.5, 0.1, 0.9 }, new double[] { 0.6, 0.4, 0.3 });
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void CalculateWarCaseFour()
+        {
+            Strategy strat = new Strategy();
+
+            var result = strat.CalculateWar(new double[] { 0.186, 0.389, 0.907, 0.832, 0.959, 0.557, 0.300, 0.992, 0.899 }, new double[] { 0.916, 0.728, 0.271, 0.520, 0.700, 0.521, 0.215, 0.341, 0.458 });
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
         public void CalculateSimpleWarLost()
         {
             Strategy strat = new Strategy();
