82d50755131de259ce6d0192ebbcd716d396dd2b
ajlopez <webmaster@ajlopez.com>
2014-05-03 13:12:39 -0300

    Repeater initial solution

---
+++ Gcj2014/Repeater/Group.cs
@@ -0,0 +1,19 @@
+﻿namespace Repeater
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Group
+    {
+        private char letter;
+        private int count;
+
+        public Group(char letter, int count)
+        {
+            this.letter = letter;
+            this.count = count;
+        }
+    }
+}
---
+++ Gcj2014/Repeater/Repeater.cs
@@ -0,0 +1,15 @@
+﻿namespace Repeater
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Repeater
+    {
+        public int Moves(IList<string> strings)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
