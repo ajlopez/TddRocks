27f6707debdff658b0d3e9fb8207039d92f68499
ajlopez <webmaster@ajlopez.com>
2014-05-03 14:03:25 -0300

    Repeater console app and sets

---
+++ Gcj2014/Repeater/Repeater.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repeater.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeater reapeter = new Repeater();
+            int ncases = int.Parse(System.Console.ReadLine());
+
+            for (int k = 1; k <= ncases; k++)
+            {
+                int nstrings = int.Parse(System.Console.ReadLine());
+                IList<string> strings = new List<string>();
+
+                for (int j = 0; j < nstrings; j++)
+                    strings.Add(System.Console.ReadLine().ToString());
+
+                int moves = reapeter.Moves(strings);
+
+                System.Console.WriteLine(string.Format("Case #{0}: {1}", k, moves < 0 ? "Fegla Won" : moves.ToString()));
+            }
+        }
+    }
+}
