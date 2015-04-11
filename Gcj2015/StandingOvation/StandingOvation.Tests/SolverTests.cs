d3078d6c61c5ef3c73bd341b24302a156e829ac5
ajlopez <webmaster@ajlopez.com>
2015-04-11 11:05:44 -0300

    Standing ovation, first resolver

---
+++ Gcj2015/StandingOvation/StandingOvation.Tests/SolverTests.cs
@@ -0,0 +1,89 @@
+﻿namespace StandingOvation.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void SolveCase1()
+        {
+            Assert.AreEqual(0, Solver.Resolve(new int[] { 1, 1, 1, 1, 1 }));
+        }
+
+        [TestMethod]
+        public void SolveCase1String()
+        {
+            Assert.AreEqual(0, Solver.Resolve("11111"));
+        }
+
+        [TestMethod]
+        public void SolveCase2()
+        {
+            Assert.AreEqual(1, Solver.Resolve(new int[] { 0, 9 }));
+        }
+
+        [TestMethod]
+        public void SolveCase2String()
+        {
+            Assert.AreEqual(1, Solver.Resolve("09"));
+        }
+
+        [TestMethod]
+        public void SolveCase3()
+        {
+            Assert.AreEqual(2, Solver.Resolve(new int[] { 1, 1, 0, 0, 1, 1 }));
+        }
+
+        [TestMethod]
+        public void SolveCase3String()
+        {
+            Assert.AreEqual(2, Solver.Resolve("110011"));
+        }
+
+        [TestMethod]
+        public void SolveCase4()
+        {
+            Assert.AreEqual(0, Solver.Resolve(new int[] { 1 }));
+        }
+
+        [TestMethod]
+        public void SolveCase4String()
+        {
+            Assert.AreEqual(0, Solver.Resolve("1"));
+        }
+
+        [TestMethod]
+        public void SolveEdgeCasesWithZero()
+        {
+            Assert.AreEqual(0, Solver.Resolve(new int[] { 2, 0, 1 }));
+            Assert.AreEqual(0, Solver.Resolve(new int[] { 3, 0, 0, 1 }));
+            Assert.AreEqual(0, Solver.Resolve(new int[] { 4, 0, 0, 0, 1 }));
+        }
+
+        [TestMethod]
+        public void SolveEdgeCasesWithZeroString()
+        {
+            Assert.AreEqual(0, Solver.Resolve("201"));
+            Assert.AreEqual(0, Solver.Resolve("3001"));
+            Assert.AreEqual(0, Solver.Resolve("40001"));
+        }
+
+        [TestMethod]
+        public void SolveEdgeCasesWithOne()
+        {
+            Assert.AreEqual(1, Solver.Resolve(new int[] { 1, 0, 1 }));
+            Assert.AreEqual(1, Solver.Resolve(new int[] { 2, 0, 0, 1 }));
+            Assert.AreEqual(1, Solver.Resolve(new int[] { 3, 0, 0, 0, 1 }));
+        }
+
+        [TestMethod]
+        public void SolveEdgeCasesWithOneString()
+        {
+            Assert.AreEqual(1, Solver.Resolve("101"));
+            Assert.AreEqual(1, Solver.Resolve("2001"));
+            Assert.AreEqual(1, Solver.Resolve("30001"));
+        }
+    }
+}
---
+++ Gcj2015/StandingOvation/StandingOvation/Solver.cs
@@ -0,0 +1,41 @@
+﻿namespace StandingOvation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Solver
+    {
+        public static int Resolve(int[] levels)
+        {
+            int result = 0;
+            int accum = levels[0];
+
+            for (int k = 1; k < levels.Length; k++)
+            {
+                if (levels[k] > 0 && accum < k)
+                {
+                    int friends = k - accum;
+                    accum += friends;
+                    result += friends;
+                }
+
+                accum += levels[k];
+            }
+
+            return result;
+        }
+
+        public static int Resolve(string levels)
+        {
+            IList<int> values = new List<int>();
+
+            for (int k = 0; k < levels.Length; k++)
+                values.Add(levels[k] - '0');
+
+            return Resolve(values.ToArray());
+        }
+    }
+}
