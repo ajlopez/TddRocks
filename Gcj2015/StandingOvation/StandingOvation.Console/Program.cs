6868753c061750d1d8f4c7d44cae4bc82e27e135
ajlopez <webmaster@ajlopez.com>
2015-04-11 11:57:02 -0300

    StandingOvation console

---
+++ Gcj2015/StandingOvation/StandingOvation.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StandingOvation.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputname = args[0];
+            string outputname = args[1];
+
+            string[] lines = File.ReadAllLines(inputname);
+
+            int ncases = int.Parse(lines[0]);
+            IList<string> results = new List<string>();
+            for (int k = 0; k < ncases; k++)
+            {
+                var words = lines[k + 1].Trim().Split(' ').ToList();
+                var solution = Solver.Resolve(words[1]);
+
+                Console.WriteLine(k + 1);
+                results.Add(string.Format("Case #{0}: {1}", k + 1, solution));
+            }
+
+            File.WriteAllLines(outputname, results.ToArray());
+        }
+    }
+}
---
+++ Gcj2015/StandingOvation/StandingOvation.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StandingOvation.Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StandingOvation.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3266913-87c1-47d5-9e2f-4bcfaed66666")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
