04fdc148141ea5d00792d7b5a3233886660f196a
ajlopez <webmaster@ajlopez.com>
2015-04-11 16:41:14 -0300

    Using average

--- Gcj2015/Pancakes/Pancakes/Solver.cs
+++ Gcj2015/Pancakes/Pancakes/Solver.cs
@@ -30,41 +30,45 @@
 
         private static IList<int> DoRound(IList<int> pancakes)
         {
-            IList<int> newpancakes = new List<int>();
+            IList<int> newpancakes1 = new List<int>();
+            IList<int> newpancakes2 = new List<int>();
 
             int maxvalue = pancakes.Max();
-            int secondmax = (maxvalue % 2 == 1) ? maxvalue / 2 + 1 : maxvalue / 2;
-            var second = pancakes.Where(n => n != maxvalue).ToList();
-            int maxvalue2 = second.Count > 0 ? second.Max() : 0;
-            int next = Math.Max(secondmax, maxvalue2);
+            int split = maxvalue / 2;
             int maxcount = pancakes.Count(n => n == maxvalue);
 
-            if (next + maxcount <= maxvalue)
-            {
-                bool found = false;
-                foreach (int value in pancakes)
-                    if (!found && value == maxvalue)
-                    {
-                        newpancakes.Add(value - secondmax);
-                        found = true;
-                    }
-                    else
-                        newpancakes.Add(value);
+            bool found = false;
+            foreach (int value in pancakes)
+                if (!found && value == maxvalue)
+                {
+                    newpancakes1.Add(value - split);
+                    found = true;
+                }
+                else
+                    newpancakes1.Add(value);
 
-                newpancakes.Add(secondmax);
-
-                return newpancakes;
-            }
+            newpancakes1.Add(split);
 
             foreach (int value in pancakes)
             {
                 int newvalue = value - 1;
 
                 if (newvalue > 0)
-                    newpancakes.Add(newvalue);
+                    newpancakes2.Add(newvalue);
             }
 
-            return newpancakes;
+            if (newpancakes1.Count == 0)
+                return newpancakes1;
+            if (newpancakes2.Count == 0)
+                return newpancakes2;
+
+            var value1 = ((double)newpancakes1.Sum()) / newpancakes1.Count;
+            var value2 = ((double)newpancakes2.Sum()) / newpancakes2.Count;
+
+            if (value1 < value2)
+                return newpancakes1;
+            else
+                return newpancakes2;
         }
     }
 }
