69d831075e95377c8fb43b53c284adfd9b352049
ajlopez <webmaster@ajlopez.com>
2015-04-11 19:20:20 -0300

    Dijkstra solver additional tests

--- Gcj2015/Dijkstra/Dijkstra.Tests/SolverTests.cs
+++ Gcj2015/Dijkstra/Dijkstra.Tests/SolverTests.cs
@@ -26,6 +26,19 @@
         public void FalseThree()
         {
             Assert.IsFalse(Solver.CanBeSolved(new Quaternions[] { Quaternions.i, Quaternions.j, Quaternions.j }, 1));
+            Assert.IsFalse(Solver.CanBeSolved(new Quaternions[] { Quaternions.k, Quaternions.j, Quaternions.i }, 1));
+        }
+
+        [TestMethod]
+        public void TrueWithRepeat()
+        {
+            Assert.IsTrue(Solver.CanBeSolved(new Quaternions[] { Quaternions.j, Quaternions.i }, 6));
+        }
+
+        [TestMethod]
+        public void FalseWithRepeat()
+        {
+            Assert.IsFalse(Solver.CanBeSolved(new Quaternions[] { Quaternions.i }, 10000));
         }
     }
 }
