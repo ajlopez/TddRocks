e3c8a6b4dd851225d9c616581c882c3a2b618d55
ajlopez <webmaster@ajlopez.com>
2015-04-11 12:34:28 -0300

    Pancakes first solver

---
+++ Gcj2015/Pancakes/Pancakes.Tests/SolverTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Pancakes.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void ResolveCase1()
+        {
+            Assert.AreEqual(3, Solver.Resolve(new int[] { 3 }));
+        }
+    }
+}
---
+++ Gcj2015/Pancakes/Pancakes/Solver.cs
@@ -0,0 +1,36 @@
+﻿namespace Pancakes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Solver
+    {
+        public static int Resolve(IList<int> pancakes)
+        {
+            int rounds = 1;
+
+            for (var newpancakes = DoRound(pancakes); newpancakes.Count > 0; newpancakes = DoRound(newpancakes))
+                rounds++;
+
+            return rounds;
+        }
+
+        private static IList<int> DoRound(IList<int> pancakes)
+        {
+            IList<int> newpancakes = new List<int>();
+
+            foreach (int value in pancakes)
+            {
+                int newvalue = value - 1;
+
+                if (newvalue > 0)
+                    newpancakes.Add(newvalue);
+            }
+
+            return newpancakes;
+        }
+    }
+}
